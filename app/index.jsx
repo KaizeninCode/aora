20bc7ea7b4ad67e32a67a3475f3de4fefe858759
Phil <philwekullo@gmail.com>
2024-12-20 05:11:51 +0300

    Resume from Onboarding Screen chapter

---
+++ app/index.jsx
@@ -0,0 +1,14 @@
+import { Text, View } from 'react-native'
+import { Link } from 'expo-router'
+
+
+const RootLayout = () => {
+  return (
+    <View className='flex-1 items-center justify-center'>
+      <Text className='text-4xl font-bold font-pblack'>Aora!</Text>
+    </View>
+  )
+}
+
+export default RootLayout
+
